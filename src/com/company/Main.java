61a6daf0875819dfca0e4999edb405178202ed9d
Sasha <cherevko.alexandr.1997@gmail.com>
2020-03-19 16:42:16 +0200

    add Stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Main {
+    private static final int Size = 10;
+    private static final int min = 10;
+    private static final int max = 20;
+    private static final int diff = (max - min) + 1;
 
     public static void main(String[] args) {
         // show me you Stream API power
         System.out.println("test");
+
+        int[] array = new  int[Size];
+        Random random = new Random();
+        for (int i = 0; i < Size; i++) {
+            array[i] = random.nextInt(diff + min);
+        }
+        IntStream streamFromArrays = Arrays.stream(array);
+        streamFromArrays.sorted().forEach(value -> System.out.print(value + " "));
     }
 
 }
